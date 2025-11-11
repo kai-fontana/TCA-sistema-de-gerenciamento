115d55a8db0859ee74186b1eb888e242d87da993
Marcos Yuri <marcos.yuriro@gmail.com>
2025-11-11 10:51:58 -0300

    Add files via upload

--- src/main/java/com/Projeto/Tca/prisma20/model/Login.java
+++ src/main/java/com/Projeto/Tca/prisma20/model/Login.java
@@ -2,21 +2,15 @@ package com.Projeto.Tca.prisma20.model;
 
 import jakarta.persistence.*;
 
-
+@Entity
 @Table(name = "cadastro")
-
 public class Login {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-
     private String email;
-
     private String senha;
 
-    public void cadastrar(){
-
-    }
 
 }
 
