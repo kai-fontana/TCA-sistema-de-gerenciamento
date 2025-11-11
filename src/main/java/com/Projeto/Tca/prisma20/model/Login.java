0a97c9cc4d3c4b2a679c249eca0dc241c5c57687
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-11-11 10:40:50 -0300

    Add files via upload

--- src/main/java/com/Projeto/Tca/prisma20/model/Login.java
+++ src/main/java/com/Projeto/Tca/prisma20/model/Login.java
@@ -1,13 +1,23 @@
 package com.Projeto.Tca.prisma20.model;
 
+import jakarta.persistence.*;
+
+
+@Table(name = "cadastro")
+
 public class Login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
     private String email;
+
     private String senha;
-    private String empresa;
 
     public void cadastrar(){
 
     }
 
 }
+
 //    private EsqueciSENHA
