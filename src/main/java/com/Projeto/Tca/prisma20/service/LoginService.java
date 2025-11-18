4f37bae3ed165eef403cacd554f67779d4aaf550
Marcos Yuri <marcos.yuriro@gmail.com>
2025-11-18 08:10:51 -0300

    Delete src/main/java/com/Projeto/Tca/prisma20/service/LoginService.java

--- src/main/java/com/Projeto/Tca/prisma20/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.Projeto.Tca.prisma20.service;
-
-import com.Projeto.Tca.prisma20.model.Login;
-
-public interface LoginService {
-
-    public boolean autenticar(String email, String senha);
-}
