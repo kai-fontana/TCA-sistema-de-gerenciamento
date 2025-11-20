59ce50b31ffe12bb5a21c4040749eb4539521d1a
Marcos Yuri <marcos.yuriro@gmail.com>
2025-11-20 09:39:52 -0300

    Delete TCA-sistema-de-gerenciamento-main/src/main/java/com/Projeto/Tca/prisma20/repository/ChamadaRepository.java

--- TCA-sistema-de-gerenciamento-main/src/main/java/com/Projeto/Tca/prisma20/repository/ChamadaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Projeto.Tca.prisma20.repository;
-
-import com.Projeto.Tca.prisma20.model.Chamada;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChamadaRepository extends JpaRepository<Chamada, Long> {
-}
