8c5a1315e0e4d49b82366360c194aa5a237c4041
Marcos Yuri <marcos.yuriro@gmail.com>
2025-11-21 11:33:38 -0300

    Add files via upload

--- src/main/java/com/Projeto/Tca/prisma20/controller/LoginController.java
+++ src/main/java/com/Projeto/Tca/prisma20/controller/LoginController.java
@@ -4,13 +4,14 @@ import com.Projeto.Tca.prisma20.service.login.LoginImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@org.springframework.stereotype.Controller
+@Controller
 @RequestMapping("/")
 public class LoginController {
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
--- src/main/java/com/Projeto/Tca/prisma20/repository/ChamadaItemRepository.java
+++ src/main/java/com/Projeto/Tca/prisma20/repository/ChamadaItemRepository.java
@@ -2,6 +2,8 @@ package com.Projeto.Tca.prisma20.repository;
 
 import com.Projeto.Tca.prisma20.model.ChamadaItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChamadaItemRepository extends JpaRepository<ChamadaItem, Long> {
 }
